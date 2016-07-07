d8f95a575804922d3c8f801255230118d8a0ba6d
Tim Dresser <tdresser@chromium.org>
2016-07-07 16:10:19 -0400

    Add RadialMenuComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { APP_SHELL_DIRECTIVES } from '@angular/app-shell';
 
+import { RadialMenuComponent } from './radial-menu.component';
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
@@ -8,9 +10,10 @@ import { APP_SHELL_DIRECTIVES } from '@angular/app-shell';
   <h1>
     {{title}}
   </h1>
+  <radial-menu></radial-menu>
   `,
   styles: [],
-  directives: [APP_SHELL_DIRECTIVES]
+  directives: [APP_SHELL_DIRECTIVES, RadialMenuComponent]
 })
 export class AppComponent {
   title = 'app works!';
---
+++ src/app/radial-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'radial-menu',
+  template: `
+  <p>This is a test</p>
+  `,
+  styles: [],
+  directives: []
+})
+export class RadialMenuComponent {
+}
